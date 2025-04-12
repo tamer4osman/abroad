935cc8beccdd0ac6569a5832063a1a40a475581f
Tamer Osman <tamer4osman@gmail.com>
2025-04-12 09:01:44 +0200

    Add FormMessage component for displaying contextual messages

---
+++ src/components/common/FormMessage.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+interface FormMessageProps {
+  message: { type: string; text: string };
+}
+
+const FormMessage: React.FC<FormMessageProps> = ({ message }) => {
+  if (!message.text)
+    return null;
+  
+
+    const messageTypeClass =
+        message.type === 'error' ? 'bg-red-100 text-red-800' : 'bg-green-100 text-green-800';
+
+    return (
+        <div className={`p-4 mb-4 ${messageTypeClass}`}>
+            {message.text}
+        </div>
+    );
+};
+
+export default FormMessage;
