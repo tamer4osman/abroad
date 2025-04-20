04b49fe87816bd2af7d89ef8ea60b6507d625ded
Tamer Osman <tamer4osman@gmail.com>
2025-04-20 16:19:44 +0200

    Add version information routes with versionController integration

---
+++ backend/src/routes/version/versionRoutes.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import { versionController } from "../../controllers/version/versionController";
+
+const router = express.Router();
+
+/**
+ * Version information routes
+ */
+router.get("/", versionController.getVersionInfo);
+router.get("/supported", versionController.getSupportedVersions);
+
+export default router;
